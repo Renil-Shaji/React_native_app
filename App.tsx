034ccac8e3126604143ab235dd3329af16beca6a
Renil-Shaji <renilshaji2003@gmail.com>
2023-08-04 10:30:09 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text,View,SafeAreaView,StyleSheet} from "react-native";
+function App() {
+  return (
+    <View>
+      <Text style={{color:"#08323"}}>
+        HELLO ALL
+      </Text>
+    </View>
+  )
+}
+export default App;
